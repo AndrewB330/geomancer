c4b9623aaa9b422d41477680ca39a69c3efbd5b2
AndrewB330 <Andrii.Borziak.330@gmail.com>
2025-03-12 12:14:06 -0700

    Convex hull implementation for 'not exact' kernels. Kernel traits reworked once more.

--- src/traits/kernel.rs
+++ src/traits/kernel.rs
@@ -8,19 +8,6 @@ use super::{FieldNumber, Point2D, RealFieldNumber};
 pub trait Kernel2D {
     type Point: Point2D<Field = Self::Field>;
     type Field: FieldNumber;
-    type RealField: RealFieldNumber + From<Self::Field> + From<f64>;
-
-    fn create_point(x: Self::Field, y: Self::Field) -> Self::Point;
-
-    fn length_sqr(a: &Self::Point) -> Self::Field;
-
-    fn distance_sqr(a: &Self::Point, b: &Self::Point) -> Self::Field;
-
-    fn dot(a: &Self::Point, b: &Self::Point) -> Self::Field;
-
-    fn cross(a: &Self::Point, b: &Self::Point) -> Self::Field;
-
-    fn cross_with_origin(a: &Self::Point, b: &Self::Point, origin: &Self::Point) -> Self::Field;
 }
 
 pub trait ExactPredicates2D: Kernel2D {
@@ -31,30 +18,31 @@ pub trait ExactPredicates2D: Kernel2D {
     fn orientation(a: &Self::Point, b: &Self::Point, c: &Self::Point) -> Orientation2D;
 }
 
-pub trait ApproxPredicates2D: Kernel2D {
-    fn compare_distance(
-        a: &Self::Point,
-        b: &Self::Point,
-        to: &Self::Point,
-        epsilon: Self::RealField,
-    ) -> Ordering;
+pub trait Operations2D: Kernel2D {
+    fn length_sqr(a: &Self::Point) -> Self::Field;
 
-    fn compare_length(a: &Self::Point, b: &Self::Point, epsilon: Self::RealField) -> Ordering;
+    fn distance_sqr(a: &Self::Point, b: &Self::Point) -> Self::Field;
 
-    fn orientation(
-        a: &Self::Point,
-        b: &Self::Point,
-        c: &Self::Point,
-        epsilon: Self::RealField,
-    ) -> Orientation2D;
+    fn dot(a: &Self::Point, b: &Self::Point) -> Self::Field;
+
+    fn dot_with_origin(a: &Self::Point, b: &Self::Point, origin: &Self::Point) -> Self::Field;
+
+    fn cross(a: &Self::Point, b: &Self::Point) -> Self::Field;
+
+    fn cross_with_origin(a: &Self::Point, b: &Self::Point, origin: &Self::Point) -> Self::Field;
 }
 
-pub trait RealFieldOperations2D: Kernel2D {
+pub trait RealOperations2D: Kernel2D + Operations2D {
+    type RealField: RealFieldNumber + From<Self::Field> + From<f32>;
     fn length(a: &Self::Point) -> Self::RealField;
 
     fn distance(a: &Self::Point, b: &Self::Point) -> Self::RealField;
 }
 
 pub trait DefaultKernel {
-    type Kernel;
+    type Kernel: Kernel2D<Point = Self>;
+}
+
+pub trait DefaultExactKernel {
+    type Kernel: Kernel2D<Point = Self>;
 }
